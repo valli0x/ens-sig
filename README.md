7fd179882b1d95cb6b3499d651740040586053c3
valli0x <youarethesame1821@gmail.com>
2023-10-28 19:38:23 +0300

    feature: added description

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Signature using ens
+
+Description
+This is a program for verifying the ECDSA signature using the Ethereum blockchain. 
+It gets a public key from the hash of the file and the signature. 
+Next, it receives a hash from the public key or an address in Ethereum. 
+Next, it accesses the Ethereum blockchain through the client 
+and receives a domain (or name) by reverse resolving. 
+Compares the entered name and the received one and outputs the result 
+of the comparison
--- boxes/root.go
+++ boxes/root.go
@@ -9,6 +9,15 @@ import (
 
 const (
 	backName = "back"
+	desc     = `
+Description
+This is a program for verifying the ECDSA signature using the Ethereum blockchain. 
+It gets a public key from the hash of the file and the signature. 
+Next, it receives a hash from the public key or an address in Ethereum. 
+Next, it accesses the Ethereum blockchain through the client 
+and receives a domain (or name) by reverse resolving. 
+Compares the entered name and the received one and outputs the result 
+of the comparison`
 )
 
 type BoxConstructor func(w fyne.Window, back *widget.Button) (name string, _ *fyne.Container, _ error)
@@ -21,6 +30,10 @@ func Root(w fyne.Window, boxList ...BoxConstructor) (*fyne.Container, error) {
 
 	head := widget.NewLabelWithStyle("Main menu", fyne.TextAlignCenter, fyne.TextStyle{Bold: true})
 	root.Add(head)
+
+	description := widget.NewLabelWithStyle(desc, fyne.TextAlignCenter, fyne.TextStyle{Bold: false, Italic: false})
+	root.Add(description)
+
 	root.Add(layout.NewSpacer())
 
 	for _, constructor := range boxList {
